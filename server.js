f2d522f4492edbb89911570ece51221031c63135
Rishika Verma <vermarishika24598@gmail.com>
2026-01-23 17:32:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // CORS config
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:1234',
+  origin: import.meta.env.FRONTEND_URL || 'http://localhost:1234',
   credentials: true,
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   allowedHeaders: ['Authorization', 'Content-Type'],
