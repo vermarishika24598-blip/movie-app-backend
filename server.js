16075bfe593d04f9643afe105b69bd6e1a7e1ebd
Rishika Verma <vermarishika24598@gmail.com>
2026-01-23 20:00:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ connectDB();
 
 const app = express();
 
-// CORS config
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:1234',
+  origin: 'http://localhost:1234',
   credentials: true,
-  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+  methods: 'GET, POST, PUT, DELETE, PATCH, HEAD',
   allowedHeaders: ['Authorization', 'Content-Type'],
 }));
+
 
 
 // Middleware
